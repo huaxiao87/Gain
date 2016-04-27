568e0575e9efe0ca434d681ec679f6f9989ee22f
Hua Xiao <shawn.huaxiao@gmail.com>
2016-04-26 20:03:41 -0400

    added ppm header and implementation

---
+++ Source/PPM.cpp
@@ -0,0 +1,134 @@
+
+#include "PPM.h"
+#include "ErrorDef.h"
+
+CPeakMeter::CPeakMeter() 
+{
+	//Do nothing, call init
+}
+
+CPeakMeter::~CPeakMeter() 
+{
+	reset();
+}
+
+Error_t CPeakMeter::init(float fSampleRate, int iNumChannels, int iBlockLength)
+{
+	m_iNumChannels = iNumChannels;
+	m_fSampleRate = fSampleRate;
+	m_iBlockLength = iBlockLength;
+	m_fLastPeak = new float[iNumChannels];
+
+	m_fMaxAmplitude = new float[iNumChannels];
+
+	// Initialize PPM Output accoring to the number of channels
+	m_PPMOutput = new float*[iNumChannels];
+
+	// Assign default values and allocate memory
+	for (int i = 0; i<iNumChannels; i++) 
+	{
+		m_fMaxAmplitude[i] = 0.0;
+		m_fLastPeak[i] = 0.0;
+		m_PPMOutput[i] = new float[m_iBlockLength];
+	}
+
+	return kNoError;
+}
+
+void CPeakMeter::reset() 
+{
+	for (int i = 0; i < m_iNumChannels; i++) 
+	{
+		delete[] m_PPMOutput[i];
+	}
+	delete[] m_fLastPeak;
+	delete[] m_fMaxAmplitude;
+	delete[] m_PPMOutput;
+}
+
+float CPeakMeter::getPeak(int iChannelNum) 
+{
+
+	float peak = m_fLastPeak[iChannelNum];
+	 
+	for (int i = 0; i < m_iBlockLength; i++) 
+	{
+		if (m_PPMOutput[iChannelNum][i] > peak) 
+		{
+			peak = m_PPMOutput[iChannelNum][i];
+		}
+	}
+
+	// Set limit to -100dB
+	if (peak < 1e-5) 
+	{
+		peak = 1e-5;
+	}
+
+	return peak;
+}
+
+float CPeakMeter::getMaxAmplitude(int iChannelNum) 
+{
+	return m_fMaxAmplitude[iChannelNum];
+}
+
+float CPeakMeter::convertFloatTodB(float fAmplitude) 
+{
+	float fAmplitudeInDB = 20 * log10(fAmplitude);
+	if (fAmplitudeInDB > 6)
+		fAmplitudeInDB = 6;
+	if (fAmplitudeInDB < -88)
+		fAmplitudeInDB = -88;
+	return fAmplitudeInDB;
+}
+
+void CPeakMeter::calculatePeak(float **pp_fAudioBlock, float fAlphaAT, float fAlphaRT) 
+{
+
+	for (int iChannelNum = 0; iChannelNum<m_iNumChannels; iChannelNum++) 
+	{
+		float fLocalPeak = 0.0;
+
+		for (int i = 0; i < m_iBlockLength; i++) 
+		{
+
+			if (m_fLastPeak[iChannelNum] > fabsf(pp_fAudioBlock[iChannelNum][i])) 
+			{
+				// Release State
+				m_PPMOutput[iChannelNum][i] = (1 - fAlphaRT) * m_fLastPeak[iChannelNum];
+			}
+			else 
+			{
+				// Attack State
+				m_PPMOutput[iChannelNum][i] = fAlphaAT * fabsf(pp_fAudioBlock[iChannelNum][i]) + (1 - fAlphaAT) * m_fLastPeak[iChannelNum];
+			}
+			if (m_PPMOutput[iChannelNum][i] > fLocalPeak) 
+			{
+				fLocalPeak = m_PPMOutput[iChannelNum][i];
+			}
+		}
+		m_fLastPeak[iChannelNum] = fLocalPeak;
+	}
+
+}
+
+void CPeakMeter::process(float **pp_fAudioBlock) 
+{
+
+	for (int c = 0; c<m_iNumChannels; c++) 
+	{
+		m_fMaxAmplitude[c] = 0.0;
+		for (int i = 0; i < m_iBlockLength; i++) 
+		{
+			float currMax = fabsf(pp_fAudioBlock[c][i]);
+			if (currMax > m_fMaxAmplitude[c]) 
+			{
+				m_fMaxAmplitude[c] = currMax;
+			}
+		}
+		// Set negative threshold to -100dB
+		if (m_fMaxAmplitude[c] < 1e-5)
+			m_fMaxAmplitude[c] = 1e-5;
+	}
+}
---
+++ inc/PPM.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdio.h>
+#include <math.h>
+#include "ErrorDef.h"
+
+class CPeakMeter
+{
+public:
+	CPeakMeter();
+	virtual ~CPeakMeter();
+	Error_t init(float fSampleRate, int iNumChannels, int iBlockLength);
+	void process(float **pp_fAudioBlock);
+	float getPeak(int iChannelNum);
+	float getMaxAmplitude(int iChannelNum);
+	float convertFloatTodB(float fAmplitude);
+	void reset();
+	void calculatePeak(float **pp_fAudioBlock, float fAlphaAT, float fAlphaRT);
+
+private:
+	float   **m_PPMOutput;
+	float   *m_fMaxAmplitude;
+	float   *m_fLastPeak;
+	int     m_iNumChannels;
+	int     m_iBlockLength;
+	float   m_fSampleRate;
+};
+
+CPeakMeter::CPeakMeter()
+{
+}
+
+CPeakMeter::~CPeakMeter()
+{
+}
